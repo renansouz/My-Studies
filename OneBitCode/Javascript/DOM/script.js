a6682af327bd6b55a44eb2abfd29be2e9cc1b6f7
renansouz <renanss2005@gmail.com>
2024-04-24 23:03:33 -0300

    learning dom

---
+++ OneBitCode/Javascript/DOM/script.js
@@ -0,0 +1,7 @@
+const newli = document.createElement('li')
+const newInput = document.createElement('input')
+newInput.type = 'text'
+newInput.name = 'input'
+
+newli.appendChild(newInput)
+ul.appendChild(newli)
