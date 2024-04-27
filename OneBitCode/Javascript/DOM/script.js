e5d96c67ed53611a34d36162eaceb27ebaea2683
renansouz <renanss2005@gmail.com>
2024-04-27 18:02:57 -0300

    manipulação do DOM

--- OneBitCode/Javascript/DOM/script.js
+++ OneBitCode/Javascript/DOM/script.js
@@ -1,7 +1,47 @@
-const newli = document.createElement('li')
-const newInput = document.createElement('input')
-newInput.type = 'text'
-newInput.name = 'input'
+function addContact() {
+  const contactSection = document.getElementById('contacts-list')
 
-newli.appendChild(newInput)
-ul.appendChild(newli)
+  const h3 = document.createElement('h3')
+  h3.innerText = "Contato"
+
+  const ul = document.createElement('ul')
+
+  const nameLi = document.createElement('li')
+  nameLi.innerText = "Nome:"
+  const nameInput = document.createElement('input')
+  nameInput.type = 'text'
+  nameInput.name = 'fullname'
+  nameLi.appendChild(nameInput)
+  ul.appendChild(nameLi)
+  ul.appendChild(document.createElement('br'))
+
+  const phoneLi = document.createElement('li')
+  phoneLi.innerText = "Telefone:"
+  const phoneInput = document.createElement('input')
+  phoneInput.type = 'text'
+  phoneInput.name = 'phone'
+  phoneLi.appendChild(phoneInput)
+  ul.appendChild(phoneLi)
+  ul.appendChild(document.createElement('br'))
+
+  const addressLi = document.createElement('li')
+  addressLi.innerHTML = '<label for="address">Endereço: </label>'
+  const addressInput = document.createElement('input')
+  addressInput.type = 'text'
+  addressInput.name = 'address'
+  addressInput.id = 'address'
+  addressLi.appendChild(addressInput)
+  ul.appendChild(addressLi)
+
+  contactSection.append(h3, ul)
+}
+
+function removeContact() {
+  const contactSection = document.getElementById('contact-list')
+
+  const titles = document.getElementsByTagName('h3')
+  const contacts = document.getElementsByTagName('ul')
+
+  contactSection.removeChild(titles[titles.length - 1])
+  contactSection.removeChild(contacts[contacts.length - 1])
+}
