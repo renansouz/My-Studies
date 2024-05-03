e29683251663693e32495e190c2643950b9bd6d5
renansouz <renanss2005@gmail.com>
2024-05-03 20:57:35 -0300

    learning rest params and spread

---
+++ OneBitCode/Javascript/modern-js/restParams.js
@@ -0,0 +1,5 @@
+function sum(...numbers) {
+  return numbers.reduce((accum, num) => accum + num, 0)
+}
+
+console.log(32, 49, 4, 1, 49);
---
+++ OneBitCode/Javascript/modern-js/spread.js
@@ -0,0 +1,11 @@
+const numbers = [1, 2, 3]
+
+const numbersCopy = numbers
+
+const numbersClone = [...numbers]
+
+const numbersObj = {...numbers}
+
+const numbersObjClone = {...numbersObj}
+
+console.log({numbers, numbersCopy, numbersClone, numbersObj, numbersObjClone});
