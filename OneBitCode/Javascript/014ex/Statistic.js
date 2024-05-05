777eda34ca78f52cfae9e1fa275229b7e6b62609
renansouz <renanss2005@gmail.com>
2024-05-04 22:58:10 -0300

    doing exercise of static

--- OneBitCode/Javascript/014ex/Statistic.js
+++ OneBitCode/Javascript/014ex/Statistic.js
@@ -1,10 +1,3 @@
-// function sum(...numbers) {
-//   return numbers.reduce((accum, num) => return accum + num)
-// }
-
-// console.log(32, 49, 4, 1, 49);
-
-
 const average = (...numbers) => {
   let sum = numbers.reduce((accum, num) =>  (accum + num), 0)
   return sum / numbers.length
@@ -23,4 +16,35 @@ const weightedAverage = (...numbers) => {
   return totalWeightedSum / totalWeight
 }
 
-console.log(weightedAverage({n: 7, p: 2}, {n: 9, p: 5}, {n: 3, p: 1}));
+// console.log(weightedAverage({n: 7, p: 2}, {n: 9, p: 5}, {n: 3, p: 1}));
+
+const median = (...numbers) => {
+  if (numbers.length % 2 === 0){
+    let mid1 = numbers.length / 2
+    let mid2 = (numbers.length /2) - 1
+    return (numbers[mid1] + numbers[mid2]) / 2 
+  } else {
+    let mid = (numbers.length / 2) - 0.5
+    return numbers[mid]
+  }
+}
+
+// console.log(median(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12));
+
+const mode = (...numbers) => {
+  let counts = {}
+  let maxCount = 0
+  let mostFrequent = numbers[0]
+
+  numbers.forEach(item => {
+    counts[item] = (counts[item] || 0) + 1;
+    if (counts[item] > maxCount) {
+      maxCount = counts[item]
+      mostFrequent = item
+    }
+  })
+
+  return mostFrequent;
+}
+
+console.log(mode(1, 1, 3, 3, 3, 5, 4, 9, 7, 4, 3, 5, 2, 4, 0, 4));
