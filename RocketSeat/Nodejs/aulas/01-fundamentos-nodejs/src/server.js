77f35def75539babbe8131214130f1012fddbacb
renansouz <renanss2005@gmail.com>
2024-05-06 18:01:10 -0300

    learning nodejs

---
+++ RocketSeat/Nodejs/aulas/01-fundamentos-nodejs/src/server.js
@@ -0,0 +1,27 @@
+import http from 'node:http'
+
+const users = []
+
+const server = http.createServer((req, res) => {
+  const { method, url }  = req
+
+  if (method === 'GET' && url === '/users'){
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users'){
+    users.push({
+      id: 1,
+      nameL: 'Joghn Doe',
+      email: 'jogndoe@example.com'
+    })
+
+    return res.writeHead(201).end()
+  }
+
+  return res.writeHead(404).end()
+})
+
+server.listen(3333)
