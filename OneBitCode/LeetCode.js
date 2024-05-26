5b9a847588912202ca6c94307dd712b30f215154
renansouz <renanss2005@gmail.com>
2024-05-26 17:32:39 -0300

    solving leet code problem

--- OneBitCode/LeetCode.js
+++ OneBitCode/LeetCode.js
@@ -1,41 +1,39 @@
-var findDuplicateSubtrees = function(root) {
-    const map = new Map();
-    const result = [];
-    
-    const traverse = (node) => {
-        if (!node) return "#";
-        const serial = `${node.val},${traverse(node.left)},${traverse(node.right)}`;
-        if (map.has(serial)) {
-            map.set(serial, map.get(serial) + 1);
-        } else {
-            map.set(serial, 1);
-        }
-        if (map.get(serial) === 2) {
-            result.push(node);
-        }
-        return serial;
-    };
-    
-    traverse(root);
-    return result;
-};
+class LRUCache {
+  constructor(capacity) {
+      this.capacity = capacity;
+      this.cache = new Map();
+  }
+
+  get(key) {
+      if (!this.cache.has(key)) {
+          return -1;
+      }
+      const value = this.cache.get(key);
+      this.cache.delete(key);
+      this.cache.set(key, value);
+      return value;
+  }
+
+  put(key, value) {
+      if (this.cache.has(key)) {
+          this.cache.delete(key);
+      }
+      this.cache.set(key, value);
+      if (this.cache.size > this.capacity) {
+          const oldestKey = this.cache.keys().next().value;
+          this.cache.delete(oldestKey);
+      }
+  }
+}
 
 // Example usage
-const tree = {
-    val: 1,
-    left: {
-        val: 2,
-        left: { val: 4, left: null, right: null },
-        right: null
-    },
-    right: {
-        val: 3,
-        left: {
-            val: 2,
-            left: { val: 4, left: null, right: null },
-            right: null
-        },
-        right: { val: 4, left: null, right: null }
-    }
-};
-console.log(findDuplicateSubtrees(tree)); // Output: [[TreeNode], [TreeNode]]
+const lruCache = new LRUCache(2);
+lruCache.put(1, 1);
+lruCache.put(2, 2);
+console.log(lruCache.get(1)); // Output: 1
+lruCache.put(3, 3);           // LRU key was 2, evicts key 2
+console.log(lruCache.get(2)); // Output: -1
+lruCache.put(4, 4);           // LRU key was 1, evicts key 1
+console.log(lruCache.get(1)); // Output: -1
+console.log(lruCache.get(3)); // Output: 3
+console.log(lruCache.get(4)); // Output: 4
