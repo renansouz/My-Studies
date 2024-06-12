fb92f1fd6ce291d4b6fcd7224795a95ed11101b8
renansouz <renanss2005@gmail.com>
2024-06-12 19:59:03 -0300

    finish the bookstore

--- OneBitCode/Javascript/OOP/bookstore/App.js
+++ OneBitCode/Javascript/OOP/bookstore/App.js
@@ -1,5 +1,8 @@
 const Database = require("./Database")
 const Author = require("./entities/Author")
+const Book = require("./entities/Book")
+const Order = require("./entities/Order")
+const Poster = require("./entities/Poster")
 const User = require("./entities/User")
 
 module.exports = class App {
@@ -11,7 +14,7 @@ module.exports = class App {
   }
 
   getUsers() {
-    App.#database.find('users')
+    return App.#database.find('users')
   }
 
   createAuthor(name, nationality, bio) {
@@ -19,7 +22,53 @@ module.exports = class App {
     App.#database.saveAuthor(author)
   }
 
-  getAuthor() {
+  getAuthors() {
     return App.#database.find('authors')
+  }
+
+  createBook(title, synopsis, genre, pages, author, description, price, inStock) {
+    const book = new Book(title, synopsis, genre, pages, author, description, price, inStock)
+    App.#database.saveBook(book)
+  }
+
+  addBook(bookName, quantity) {
+    App.#database.addBooksToStock(bookName, quantity)
+  }
+
+  getBooks() {
+    return App.#database.find('books')
+  }
+
+  createPoster(name, description, height, width, price, inStock) {
+    const poster = new Poster(name, description, height, width, price, inStock)
+    App.#database.savePoster(poster)
+  }
+
+  addPoster(postername, quantity) {
+    App.#database.addPostersToStock(postername, quantity)
+  }
+
+  getPosters() {
+    return App.#database.find('posters')
+  }
+
+  createOrder(items, user) {
+    const order = new Order(items, user)
+    App.#database.saveOrder(order)
+    order.data.items.forEach(({ product, quantity }) => {
+      if (product instanceof Book) {
+        App.#database.removeBooksFromStock(product.name, quantity)
+      } else if (product instanceof Poster) {
+        App.#database.removePostersFromStock(product.name, quantity)
+      }
+    })
+  }
+
+  getOrders() {
+    return App.#database.find('orders')
+  }
+
+  showDatabase() {
+    App.#database.showStorage()
   }
 }
--- OneBitCode/Javascript/OOP/bookstore/entities/User.js
+++ OneBitCode/Javascript/OOP/bookstore/entities/User.js
@@ -1,4 +1,4 @@
-module.experts = class User {
+module.exports = class User {
   constructor(name, email, password) {
     this.name = name
     this.email = email
