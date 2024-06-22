55d0512ff1b89a380f952cbeed06f813226180b4
renansouz <renanss2005@gmail.com>
2024-06-21 22:37:14 -0300

    validator for the sign up with regex

--- OneBitCode/Javascript/023ex/validator.js
+++ OneBitCode/Javascript/023ex/validator.js
@@ -1,16 +1,51 @@
-const email = document.getElementById('email').value
-const errorElement = document.querySelector('.error')
-
-let emailRegex = /^[a-zA-Z0-9._%+-]{2,}@[a-zA-Z0-9.-]{2,}+\.[a-zA-Z]{2,}$/
-
-let errors = []
-
-if (!emailRegex.test(email)) {
-  errors.push('email inválido.')
+function validateEmail(email) {
+  if(!email.match(/\w{2,}@[a-zA-Z]{2,}\.[a-zA-Z]{2,}/)){
+    const err = new Error('Invalid email.')
+    err.input = 'email'
+    throw err
+  }
 }
 
-if (errors.lenght > 0) {
-  errorElement.textContent = errors.join(' ')
-} else {
-  errorElement.textContent = ''
+function validatePassword(password) {
+  if (
+    password.lenght < 8 || 
+    !password.match(/[a-z]/) ||
+    !password.match(/[A-Z]/) ||
+    !password.match(/[\d]/) ||
+    !password.match(/[^a-zA-Z\s0-9]/)
+  ) {
+    const err = new Error('Invalid password')
+    err.input = 'password'
+    throw err
+  }
 }
+
+function resetFormStyles(inputs) {
+  Object.entries(inputs).forEach(([key, value]) => {
+      value.classList.remove('succes', 'error')
+      document.querySelector(`#${key}-error`).textContent = ''
+  })
+}
+
+const userInputs = {
+  name: document.querySelector('#name'),
+  email: document.querySelector('#email'),
+  password: document.querySelector('#password')
+}
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  resetFormStyles(userInputs)
+  try{
+    userInputs.name.classList.add('success')
+    validateEmail(userInputs.email.value)
+    userInputs.email.classList.add('success')
+    validatePassword(userInputs.password.value)
+    userInputs.password.classList.add('success')
+  } catch (err) {
+    userInputs[err.input].classList.add('error')
+    document.querySelector(`#${err.input}-error`).textContent = err.message
+  }
+})
