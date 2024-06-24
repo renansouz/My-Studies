8b2b6945a486db80d8dfe42c91f8b83db730346d
renansouz <renanss2005@gmail.com>
2024-06-24 18:49:18 -0300

    learn promises

--- OneBitCode/Javascript/advanced-js/promises/index.js
+++ OneBitCode/Javascript/advanced-js/promises/index.js
@@ -1,17 +1,39 @@
-// pending
-// resolved
-// rejected
-// finalizada
-const p = new Promise((resolve) => {
-  console.log(`A promise está sendo executada.`);
-  setTimeout(() => {
-    console.log(`resolvendo a promisse`);
-    resolve(true)
-  }, 2000);
-})
+// // pending
+// // resolved
+// // rejected
+// // finalizada
+// const p = new Promise((resolve, reject) => {
+//   console.log(`A promise está sendo executada.`);
+//   setTimeout(() => {
+//     if(true) {
+//       reject(false)
+//     }
+//     console.log(`resolvendo a promisse`);
+//     resolve(true)
+//   }, 2000);
+// })
+
+// console.log(p);
+
+// setTimeout(() => {
+//   console.log(p);
+// }, 3000);
+
+  
+function execute() {
+  return new Promise ((resolve, reject) => {
+    console.log(`A promise está sendo executada...`);
+    setTimeout(() => {
+      console.log(`Resolvendo a promise...`);
+      resolve('resultado')
+    })
+  })
+}  
+  
+const p = execute()
 
 console.log(p);
 
 setTimeout(() => {
   console.log(p);
-}, 3000);
+}, 2000)
