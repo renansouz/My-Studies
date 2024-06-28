fda4f9b24ecfa655bdba44d2745c5b62740b7369
renansouz <renanss2005@gmail.com>
2024-06-27 21:02:17 -0300

    one more leetcode exercise

--- OneBitCode/LeetCode.js
+++ OneBitCode/LeetCode.js
@@ -1,24 +1,21 @@
-/**
- * @param {number[]} height
- * @return {number}
- */
-var maxArea = function(height) {
-  let left = 0;
-  let right = height.length - 1;
-  let maxArea = 0;
+function lengthOfLongestSubstring(s) {
+    let n = s.length;
+    let set = new Set();
+    let maxLength = 0, i = 0, j = 0;
+    
+    while (i < n && j < n) {
+        if (!set.has(s[j])) {
+            set.add(s[j++]);
+            maxLength = Math.max(maxLength, j - i);
+        } else {
+            set.delete(s[i++]);
+        }
+    }
+    
+    return maxLength;
+}
 
-  while (left < right) {
-      let width = right - left;
-      let currentHeight = Math.min(height[left], height[right]);
-      let area = currentHeight * width;
-      maxArea = Math.max(maxArea, area);
-
-      if (height[left] < height[right]) {
-          left++;
-      } else {
-          right--;
-      }
-  }
-
-  return maxArea;
-};
+// Test cases
+console.log(lengthOfLongestSubstring("abcabcbb")); // Output: 3
+console.log(lengthOfLongestSubstring("bbbbb"));    // Output: 1
+console.log(lengthOfLongestSubstring("pwwkew"));   // Output: 3
