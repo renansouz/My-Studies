3606a6423d5615f10a3465ee7c7b730d4e66c08c
renansouz <renanss2005@gmail.com>
2024-12-10 18:21:43 -0800

    array in greystone

---
+++ ACADEMIC-LEARNING/GreyStone/JS/loops/005ex.js
@@ -0,0 +1,7 @@
+function displayValue() {
+  let stdName = ["A", "B", "C", "D", "E", "F", "G"]
+  for (let i = 0; i < stdName.length; i++) {
+    const element = stdName[i];
+    console.log(element);
+  }
+}
