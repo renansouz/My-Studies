986af0eb7860ae599e45de85ecba43ab7ff9668b
renansouz <renanss2005@gmail.com>
2025-01-01 21:53:59 -0800

    HAPPY NEW YEAR!!!

---
+++ ACADEMIC-LEARNING/OneBitCode/Javascript/Happy-new-Year/script.js
@@ -0,0 +1,16 @@
+let newYearLuck = () => {
+  const wishes = [
+        "Success",
+        "Health",
+        "Happiness",
+        "Growth",
+        "Creativity",
+        "Love",
+        "Abundance"
+  ]
+
+  console.log("Wishing you a fantastic year ahead!!!");
+  wishes.forEach(wish => console.log(`${wish} is coming your way!`))
+}
+
+newYearLuck()
