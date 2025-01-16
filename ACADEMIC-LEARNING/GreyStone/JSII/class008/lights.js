13f472532eb18117dcb6d5a545a19dd102364eaa
renansouz <renanss2005@gmail.com>
2025-01-15 20:52:42 -0800

    learn set interval

---
+++ ACADEMIC-LEARNING/GreyStone/JSII/class008/lights.js
@@ -0,0 +1,11 @@
+let arr = ['red','blue','green','black']
+
+let colors = range => {
+  const random = Math.floor(Math.random() * range)
+  console.log(trash);
+  document.body.style.backgroundColor = arr[random]
+}
+
+
+
+setInterval(colors, 1000, 2)
---
+++ ACADEMIC-LEARNING/GreyStone/JSII/class008/script.js
@@ -0,0 +1,12 @@
+const arr = [-3, -2, -1, 0, 1, 2, 3, 4,
+  5, 6, 7, 8, 9, 10, 11, 12, 13];
+  
+  function isPrime(num) {
+    for (let i = 2; i < num; i++) {
+      if (num % i == 0) return false;
+    }
+  
+    return num > 1;
+  }
+  
+  console.log(arr.filter(isPrime)); // [2, 3, 5, 7, 11, 13]
