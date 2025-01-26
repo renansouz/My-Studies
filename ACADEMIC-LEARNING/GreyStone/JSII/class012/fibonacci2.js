61c250af89d9482e9950269534072afa876251fe
renansouz <renanss2005@gmail.com>
2025-01-25 23:35:04 -0800

    fibonacci

---
+++ ACADEMIC-LEARNING/GreyStone/JSII/class012/fibonacci2.js
@@ -0,0 +1,11 @@
+function fibonacci(n) {
+  if (n <= 1) return n;
+  let a = 0, b = 1;
+  for (let i = 2; i <= n; i++) {
+    [a, b] = [b, a + b];
+  }
+  console.log(b);
+  return b;
+}
+
+fibonacci(5)
