f7879a0fbec6386fb43a2a7e6acd9a00c5375b2d
renansouz <renanss2005@gmail.com>
2025-02-11 23:51:45 -0800

    optimizing the code

--- FRONTEND/leetcode/1.TwoSum/twosum.js
+++ FRONTEND/leetcode/1.TwoSum/twosum.js
@@ -1,6 +1,6 @@
 var twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++) {
-        for (let j = 1; i < nums.length; j++) {
+        for (let j = 1; j < nums.length; j++) {
             if (nums[i] + nums[i+j] === target) {
                 console.log([i, i+j] + "sim");
                 return [i, i+j]
@@ -10,3 +10,23 @@ var twoSum = function(nums, target) {
 };
 
 twoSum([3,2,3], 6)
+
+//optimized way
+
+// var twoSum = function(nums, target) {
+//     const map = new Map(); // Create a hash map to store numbers and their indices
+
+//     for (let i = 0; i < nums.length; i++) {
+//         const complement = target - nums[i]; // Calculate the complement
+        
+//         // Check if the complement exists in the map
+//         if (map.has(complement)) {
+//             return [map.get(complement), i]; // If found, return the indices
+//         }
+        
+//         // If not, store the current number with its index
+//         map.set(nums[i], i);
+//     }
+// };
+
+// console.log(twoSum([3, 2, 3], 6));
