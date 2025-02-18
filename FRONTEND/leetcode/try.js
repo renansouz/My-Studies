064d89369f0c493db8c98dc9e33780f51f3abaf4
renansouz <renanss2005@gmail.com>
2025-02-18 00:34:29 -0800

    never stop the practice

--- FRONTEND/leetcode/try.js
+++ FRONTEND/leetcode/try.js
@@ -1,9 +1,20 @@
-function narcissistic(value) {
-  console.log( ('' + value)
-    .split("")
-    .map(n => Number(n) ** ('' + value).length).reduce((a,b) => a+b) == value)
-}
-let string = "153"
-let number = 153
-
-console.log(string == number)
+let romanToInt = roman => {
+  const romanMap = {
+    'M':1000,
+    'D':500,
+    'C':100,
+    'L':50,
+    'X':10,
+    'V':5,
+    'I':1
+  }
+    let  total = 0;
+    for(let i = 0; i < roman.length; i++){
+        if(romanMap[roman[i]] < romanMap[roman[i+1]]) {
+            total -= romanMap[roman[i]] 
+        } else {
+             total += romanMap[roman[i]] 
+        }
+    }
+    return total
+  }
