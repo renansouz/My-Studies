1873c61eda0824cf19b8b36f8508be1df5b5de15
renansouz <renanss2005@gmail.com>
2025-03-03 22:51:18 -0800

    learning the correct way to divide a component

--- FRONTEND/react-udemy/01-starting-project/src/components/Header/Header.jsx
+++ FRONTEND/react-udemy/01-starting-project/src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
+import reactImg from "../../assets/react-core-concepts.png";
+import "./Header.css";
+
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
-function Header() {
+export default function Header() {
   const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
