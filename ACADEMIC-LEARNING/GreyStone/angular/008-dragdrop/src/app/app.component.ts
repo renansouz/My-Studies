e377c31dfc3640933580c4d2008d8e2017d5fc4f
renansouz <renanss2005@gmail.com>
2025-03-15 13:14:57 -0700

    done drag and drop

--- ACADEMIC-LEARNING/GreyStone/angular/008-dragdrop/src/app/app.component.ts
+++ ACADEMIC-LEARNING/GreyStone/angular/008-dragdrop/src/app/app.component.ts
@@ -12,6 +12,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
@@ -23,6 +24,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatIconModule,
   ],
   templateUrl: './app.component.html',
   styles: [
@@ -111,5 +113,9 @@ export class AppComponent {
       this.taskTitle = '';
       this.taskDescription = '';
     }
+  }
+  deleteTask(task: { title: string; description: string }) {
+    this.todo = this.todo.filter((t) => t.title !== task.title);
+    this.done = this.done.filter((t) => t.title !== task.title);
   }
 }
