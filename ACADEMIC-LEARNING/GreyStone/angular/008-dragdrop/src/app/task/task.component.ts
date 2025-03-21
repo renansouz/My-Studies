8ed04447a5d50ec773087cf3bf75d4de419a360f
renansouz <renanss2005@gmail.com>
2025-03-20 20:07:29 -0700

    saving before change for dialog

---
+++ ACADEMIC-LEARNING/GreyStone/angular/008-dragdrop/src/app/task/task.component.ts
@@ -0,0 +1,120 @@
+import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  CdkDrag,
+  CdkDropList,
+  CdkDropListGroup,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-task',
+  imports: [
+    CdkDropListGroup,
+    CdkDropList,
+    CdkDrag,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  templateUrl: './task.component.html',
+  styles: [
+    `
+      .example-container {
+        width: 400px;
+        max-width: 100%;
+        margin: 0 25px 25px 0;
+        display: inline-block;
+        vertical-align: top;
+      }
+
+      .example-list {
+        border: solid 1px #ccc;
+        min-height: 60px;
+        background: white;
+        border-radius: 4px;
+        overflow: hidden;
+        display: block;
+      }
+
+      .example-box {
+        padding: 20px 10px;
+        border-bottom: solid 1px #ccc;
+        color: rgba(0, 0, 0, 0.87);
+        align-items: center;
+        cursor: move;
+        font-size: 14px;
+      }
+
+      .cdk-drag-preview {
+        border-radius: 4px;
+        box-shadow: 0 5px 5px -3px rgba(0, 0, 0, 0.2),
+          0 8px 10px 1px rgba(0, 0, 0, 0.14), 0 3px 14px 2px rgba(0, 0, 0, 0.12);
+      }
+
+      .cdk-drag-placeholder {
+        opacity: 0;
+      }
+
+      .cdk-drag-animating {
+        transition: transform 250ms cubic-bezier(0, 0, 0.2, 1);
+      }
+
+      .example-box:last-child {
+        border: none;
+      }
+
+      .example-list.cdk-drop-list-dragging
+        .example-box:not(.cdk-drag-placeholder) {
+        transition: transform 250ms cubic-bezier(0, 0, 0.2, 1);
+      }
+    `,
+  ],
+})
+export class TaskComponent {
+  todo: { title: string; description: string }[] = [];
+  done: { title: string; description: string }[] = [];
+
+  taskTitle: string = '';
+  taskDescription: string = '';
+
+  drop(event: CdkDragDrop<{ title: string; description: string }[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+
+  addTask() {
+    if (this.taskTitle && this.taskDescription) {
+      this.todo.push({
+        title: this.taskTitle,
+        description: this.taskDescription,
+      });
+      this.taskTitle = '';
+      this.taskDescription = '';
+    }
+  }
+  deleteTask(task: { title: string; description: string }) {
+    this.todo = this.todo.filter((t) => t.title !== task.title);
+    this.done = this.done.filter((t) => t.title !== task.title);
+  }
+}
