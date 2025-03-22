f10874dc2209b34b7611dc6401f30912295502cb
renansouz <renanss2005@gmail.com>
2025-03-21 23:44:00 -0700

    add dialog, add backlog / in progess

---
+++ ACADEMIC-LEARNING/GreyStone/angular/008-dragdrop/src/app/task-dialog/task-dialog.component.ts
@@ -0,0 +1,35 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-task-dialog',
+  imports: [
+    MatDialogModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
+  templateUrl: './task-dialog.component.html',
+  styles: ``,
+})
+export class TaskDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<TaskDialogComponent>,
+    @Inject(MAT_DIALOG_DATA)
+    public data: { taskTitle: string; taskDescription: string }
+  ) {}
+
+  onCancel(): void {
+    this.dialogRef.close();
+  }
+
+  onOk(): void {
+    this.dialogRef.close(this.data);
+  }
+}
