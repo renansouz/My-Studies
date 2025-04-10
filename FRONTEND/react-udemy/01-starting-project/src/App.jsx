a1370b35d77927b31b1bff6871cd0325e2e8093f
renansouz <renanss2005@gmail.com>
2025-04-09 22:12:56 -0700

    Update App.jsx to render CoreConcepts dynamically and add TabButton functionality

--- FRONTEND/react-udemy/01-starting-project/src/App.jsx
+++ FRONTEND/react-udemy/01-starting-project/src/App.jsx
@@ -1,12 +1,15 @@
-import { CORE_CONCEPTS } from "./data.js";
+import { useState } from "react";
+
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcepts from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  let tabContent = "Please c lick a button";
+  const [selectedTopic, setSelectedTopic] = useState("components");
+
   function handleSelect(selectedButton) {
-    tabContent = selectedButton;
+    setSelectedTopic(selectedButton);
   }
 
   return (
@@ -32,7 +35,13 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {tabContent}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
