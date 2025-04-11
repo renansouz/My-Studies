2fc19d90c8feb9176b3c4e4605503f52d2656393
renansouz <renanss2005@gmail.com>
2025-04-10 22:02:53 -0700

    test(index): add createElement for testing purposes

--- FRONTEND/react-udemy/01-starting-project/src/index.jsx
+++ FRONTEND/react-udemy/01-starting-project/src/index.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
@@ -5,3 +6,4 @@ import "./index.css";
 
 const entryPoint = document.getElementById("root");
 ReactDOM.createRoot(entryPoint).render(<App />);
+// ReactDOM.createRoot(entryPoint).render(React.createElement);
