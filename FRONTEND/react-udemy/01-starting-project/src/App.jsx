0d296c9010ccaddbef337fecbacc02c84a51c6e2
renansouz <renanss2005@gmail.com>
2025-04-10 21:59:52 -0700

    refactor: update App component with sections separated in components

--- FRONTEND/react-udemy/01-starting-project/src/App.jsx
+++ FRONTEND/react-udemy/01-starting-project/src/App.jsx
@@ -1,50 +1,16 @@
-import { useState } from "react";
-
-import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
+import CoreConcepts from "./components/CoreConcepts.jsx";
 import Header from "./components/Header/Header.jsx";
-import CoreConcepts from "./components/CoreConcept.jsx";
-import TabButton from "./components/TabButton.jsx";
+import Examples from "./components/Examples.jsx";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("components");
-
-  function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton);
-  }
-
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            <CoreConcepts {...CORE_CONCEPTS[0]} />
-            <CoreConcepts {...CORE_CONCEPTS[1]} />
-            <CoreConcepts {...CORE_CONCEPTS[2]} />
-            <CoreConcepts {...CORE_CONCEPTS[3]} />
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton onSelect={() => handleSelect("components")}>
-              Components
-            </TabButton>
-            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
-          </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
-    </div>
+    </>
   );
 }
 
