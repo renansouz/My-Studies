eb56467d3b94ea5b112c9a25c846e06b7ba5eef7
renansouz <renanss2005@gmail.com>
2025-04-14 19:44:04 -0700

    feat(App): Integrate GameBoard and Player components
    Connected the GameBoard and Player components to the main App layout

--- FRONTEND/react-udemy/07-tic-tac-toe-starting-project/src/App.jsx
+++ FRONTEND/react-udemy/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,5 +1,19 @@
+import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
+
 function App() {
-  return <p>Coming soon...</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbol="x" />
+          <Player initialName="Player 2" symbol="o" />
+        </ol>
+        <GameBoard />
+      </div>
+      LOG
+    </main>
+  );
 }
 
 export default App;
