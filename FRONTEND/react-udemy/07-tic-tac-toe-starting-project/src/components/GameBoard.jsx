5aec7b9d1de9cb0ee556d801f65921f844d6506b
renansouz <renanss2005@gmail.com>
2025-04-16 22:44:02 -0700

    refactor(GameBoard): Remove internal state and refactor to accept props
    Moved state management to the main application to allow sharing across components. Regactored GameBoard to rely on props for rendering  and interaction.

--- FRONTEND/react-udemy/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ FRONTEND/react-udemy/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,22 +1,17 @@
-import { useState } from "react";
-
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard() {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initialGameBoard;
 
-  function handleSelectSquare(rowIndex, colIndex) {
-    setGameBoard((prevGameBoard) => {
-      let updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      updatedBoard[rowIndex][colIndex] = "X";
-      return updatedBoard;
-    });
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
   }
 
   return (
@@ -26,7 +21,7 @@ export default function GameBoard() {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
                   {playerSymbol}
                 </button>
               </li>
