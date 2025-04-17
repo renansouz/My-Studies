40223cf1776a2ccbf494a941c2904ddacf39d8ff
renansouz <renanss2005@gmail.com>
2025-04-16 22:39:31 -0700

    feat(Player): Add isActive prop to highlight active player
    Added the isActive prop and applied it to the <li> element to visually indicate the active player

--- FRONTEND/react-udemy/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ FRONTEND/react-udemy/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
@@ -22,7 +22,7 @@ export default function Player({ initialName, symbol }) {
   }
 
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
