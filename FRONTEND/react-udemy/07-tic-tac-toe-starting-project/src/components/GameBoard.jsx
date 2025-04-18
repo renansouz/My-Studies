6d931af38a8b1148c028b9c6792e1f0ddc54f697
renansouz <renanss2005@gmail.com>
2025-04-17 22:47:36 -0700

    feat(GameBoard): Disable button after square is selected
    Updated the button to become disabled if a square is already selected, preventing multiple clicks.

--- FRONTEND/react-udemy/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ FRONTEND/react-udemy/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -21,7 +21,10 @@ export default function GameBoard({ onSelectSquare, turns }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
                   {playerSymbol}
                 </button>
               </li>
