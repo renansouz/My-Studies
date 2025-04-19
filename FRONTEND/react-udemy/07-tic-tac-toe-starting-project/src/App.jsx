7d8f9f6668ad1808dadfa46d2504ba21e52bd3b2
renansouz <renanss2005@gmail.com>
2025-04-18 23:59:41 -0700

    feat(App): brought all variables and functionalities of the board fom gameboardjsx to appjsx and starting game validation

--- FRONTEND/react-udemy/07-tic-tac-toe-starting-project/src/App.jsx
+++ FRONTEND/react-udemy/07-tic-tac-toe-starting-project/src/App.jsx
@@ -6,6 +6,12 @@ import Log from "./components/Log";
 
 import { WINNING_COMBINATIONS } from "./components/winning-combination";
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
 
@@ -18,6 +24,19 @@ function App() {
   const [gameTurns, setGameTurns] = useState([]);
 
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  for (const combination of WINNING_COMBINATIONS) {
+    //todo
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
@@ -47,7 +66,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
