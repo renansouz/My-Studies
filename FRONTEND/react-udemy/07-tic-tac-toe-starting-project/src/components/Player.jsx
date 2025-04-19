cb3f2af274641433a436aac9dcf252aacd0c9475
renansouz <renanss2005@gmail.com>
2025-04-19 13:00:36 -0700

    feat(Player): Add onChangeName functionality
    Implemented the OnChangeName prop to allow updating player names dynamically.

--- FRONTEND/react-udemy/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ FRONTEND/react-udemy/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,10 +1,19 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     setIsEditing((editing) => !editing);
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
 
   const [playerName, setPlayerName] = useState(initialName);
