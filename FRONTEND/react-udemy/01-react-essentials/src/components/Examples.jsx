925dccdecea732b283c82004c372b85a8851e817
renansouz <renanss2005@gmail.com>
2025-04-20 19:00:23 -0700

    refactor: rename file to 01-react-essentials

---
+++ FRONTEND/react-udemy/01-react-essentials/src/components/Examples.jsx
@@ -0,0 +1,64 @@
+import { EXAMPLES } from "../data.js";
+import { useState } from "react";
+
+import TabButton from "./TabButton.jsx";
+import Section from "./Section.jsx";
+
+export default function Examples() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+  return (
+    <Section title="Examples" id="examples">
+      <Tabs
+        buttonsContainer="menu"
+        buttons={
+          <>
+            <TabButton
+              isSelected={selectedTopic == "components"}
+              onClick={() => handleSelect("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic == "jsx"}
+              onClick={() => handleSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic == "props"}
+              onClick={() => handleSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic == "state"}
+              onClick={() => handleSelect("state")}
+            >
+              State
+            </TabButton>
+          </>
+        }
+      >
+        {tabContent}
+      </Tabs>
+    </Section>
+  );
+}
---
+++ FRONTEND/react-udemy/01-react-essentials/src/components/TabButton.jsx
@@ -0,0 +1,9 @@
+export default function TabButton({ children, isSelected, ...props }) {
+  return (
+    <li>
+      <button className={isSelected ? "active" : undefined} {...props}>
+        {children}
+      </button>
+    </li>
+  );
+}
