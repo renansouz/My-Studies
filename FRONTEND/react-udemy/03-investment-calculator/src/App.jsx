288a93d98eec39fc480aa5241dc605e03025c662
renansouz <renanss2005@gmail.com>
2025-04-21 22:49:00 -0700

    feat(App): integrate Header component into app

--- FRONTEND/react-udemy/03-investment-calculator/src/App.jsx
+++ FRONTEND/react-udemy/03-investment-calculator/src/App.jsx
@@ -1,7 +1,11 @@
+import Header from "./components/Header.jsx";
+
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+    </>
+  );
 }
 
-export default App
+export default App;
