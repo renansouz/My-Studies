2e74eaef242bf92c0f050fd16002258d4f82c7c9
renansouz <renanss2005@gmail.com>
2025-05-13 22:57:05 -0700

    feat: dynamic & conditional styling with styled component.
    feat: starting a new project in react greystone

--- FRONTEND/react-udemy/04-styling-react-app/src/components/AuthInputs.jsx
+++ FRONTEND/react-udemy/04-styling-react-app/src/components/AuthInputs.jsx
@@ -15,18 +15,19 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: #6b7280;
+  color: ${({invalid}) => invalid ? '#f87171' : '#6b7280'};
 `;
 
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: #d1d5db;
   color: #374151;
   border: 1px solid transparent;
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+  background-color: ${({invalid}) => invalid ? '#fed2d2' : '#d1d5db'};
+  color: ${({invalid}) => invalid ? '#ef4444' : '#374151'};
 `;
 
 export default function AuthInputs() {
@@ -53,22 +54,20 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlContainer>
         <p>
-          <Label className={`label ${emailNotValid} ? 'invalid' : ''`}>
+          <Label invalid={emailNotValid}>
             Email
           </Label>
           <Input
             type="email"
-            className={emailNotValid ? "invalid" : undefined}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <Label className={`label ${emailNotValid} ? 'invalid' : ''`}>
+          <Label invalid={emailNotValid}>
             Password
           </Label>
           <Input
             type="password"
-            className={passwordNotValid ? "invalid" : undefined}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
