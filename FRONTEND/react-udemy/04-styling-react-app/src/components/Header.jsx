14fb8e4f5cf6152c62cc86586e12b4788b177fc6
renansouz <renanss2005@gmail.com>
2025-05-16 22:59:07 -0700

    feat: deleting the styled components and seeing how to do in tailwind

--- FRONTEND/react-udemy/04-styling-react-app/src/components/Header.jsx
+++ FRONTEND/react-udemy/04-styling-react-app/src/components/Header.jsx
@@ -1,55 +1,15 @@
-import { styled } from "styled-components";
-
 import logo from "../assets/logo.png";
-// import "./Header.css";
-
-const StyledHeader = styled.header`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-top: 2rem;
-  margin-bottom: 2rem;
-
-  & img {
-    object-fit: contain;
-    margin-bottom: 2rem;
-    width: 11rem;
-    height: 11rem;
-  }
-
-  & h1 {
-    font-size: 1.5rem;
-    font-weight: 600;
-    letter-spacing: 0.4em;
-    text-align: center;
-    text-transform: uppercase;
-    color: #9a3412;
-    font-family: "Pacifico", cursive;
-    margin: 0;
-  }
-
-  & p {
-    text-align: center;
-    color: #a39191;
-    margin: 0;
-  }
-
-  @media (min-width: 768px) {
-    margin-bottom: 4rem;
-
-    & h1 {
-      font-size: 2.25rem;
-    }
-  }
-`;
 
 export default function Header() {
   return (
-    <StyledHeader>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
+    <header className="flex flex-col items-center mt-8 mb-16">
+      <img
+        src={logo}
+        alt="A canvas"
+        className="mb-8 w-44 h-44 object-contain"
+      />
+      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800">ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
-    </StyledHeader>
+    </header>
   );
 }
